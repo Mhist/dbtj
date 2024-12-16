e62b6b9a98c8cdbc66ff1ad91277060fe6d630da
jiang yadong <18827637106@163.com>
2024-12-16 15:32:55 +0800

    feat:购物车

--- src/pages/tab/basket/basket.vue
+++ src/pages/tab/basket/basket.vue
@@ -1,60 +1,481 @@
 <template>
   <view class="xxx-content">
     <view class="good-wrap">
-      <view class="single-good" v-for="i in 10" :key="i">
-        <view class="single-select"></view>
-        <view class="single-image"></view>
-        <view class="single-detail"></view>
-        <view class="single-operation"></view>
+      <view
+        v-for="(item) in baseketList"
+        :key="item.id"
+        class="single-good"
+      >
+        <view class="single-select">
+          <up-checkbox
+            v-model:checked="item.checked"
+            :custom-style="{}"
+            used-alone
+            shape="circle"
+            active-color="#07c160"
+            bg-color="#fff"
+          />
+        </view>
+        <view class="single-image">
+          <up-image
+            class="image"
+            :show-loading="true"
+            :src="item.imgSrc"
+            width="113rpx"
+            height="113rpx"
+          />
+        </view>
+        <view class="single-detail">
+          <view class="good-name up-line-1">
+            {{ item.name }}
+          </view>
+          <view class="good-sku up-line-1">
+            {{ item.sku }}
+          </view>
+          <view class="good-price up-line-1">
+            <text class="good-price-label">
+              ¥
+            </text> {{ item.price }}
+          </view>
+        </view>
+        <view class="single-operation">
+          <view class="delete">
+            移除
+          </view>
+          <view class="amount">
+            <up-number-box
+              v-model="item.amount"
+              integer
+              button-size="5"
+              color="#ffffff"
+              bg-color="#f2f2f2"
+              icon-style="color: #fff"
+              :min="1"
+              :max="100"
+            >
+              <template #minus>
+                <view class="minus amount-icon-view">
+                  <up-icon class="amount-icon" name="minus" size="5" />
+                </view>
+              </template>
+              <template #input>
+                <text
+                  style="
+                    width: 20px;
+                    text-align: center;
+                    color: #333;
+                    font-size: 19rpx;
+                  "
+                  class="input"
+                >
+                  {{ item.amount }}
+                </text>
+              </template>
+              <template #plus>
+                <view class="plus amount-icon-view">
+                  <up-icon class="amount-icon" name="plus" size="5" />
+                </view>
+              </template>
+            </up-number-box>
+          </view>
+        </view>
       </view>
     </view>
     <view class="collect-wrap">
-
+      <view class="select-all">
+        <up-checkbox
+          v-model:checked="isAllChecked"
+          :custom-style="{}"
+          used-alone
+          shape="circle"
+          active-color="#07c160"
+          label-color="#333"
+          label=""
+        /><text>全选</text>
+      </view>
+      <view class="price-all">
+        <text class="price-all-label">
+          合计:
+        </text>
+        <text class="good-price-label">
+          ¥
+        </text>
+        <text class="price-all-value">
+          {{ priceTotal }}
+        </text>
+      </view>
+      <view class="handle-all">
+        <up-button
+          style="color: #333"
+          class="handle-button"
+          color="#d5d5d5"
+          shape="circle"
+          text="去结算"
+        />
+      </view>
     </view>
   </view>
 </template>
 
 <script setup lang="js">
-  import {
-    reactive,
-    ref,
-  } from 'vue';
+import {
+  reactive,
+  ref,
+} from 'vue';
+
+const baseketList = ref([]);
+const priceTotal = ref(600);
+const isAllChecked = ref(false);
+
+const loadData = () => {
+  baseketList.value = [{
+    id: 1,
+    checked: false,
+    name: '可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐',
+    price: 100,
+    sku: '500ml+礼盒装',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/1.jpg',
+  }, {
+    id: 2,
+    checked: false,
+    name: '雪碧',
+    price: 105,
+    sku: '1L',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/2.jpg',
+  }, {
+    id: 3,
+    checked: false,
+    name: '矿泉水',
+    price: 95,
+    sku: '330ml',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/3.jpg',
+  }, {
+    id: 4,
+    checked: false,
+    name: '绿茶',
+    price: 110,
+    sku: '500ml+礼盒装',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/4.jpg',
+  }, {
+    id: 5,
+    checked: false,
+    name: '红茶',
+    price: 115,
+    sku: '1.5L',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/5.jpg',
+  }, {
+    id: 6,
+    checked: false,
+    name: '咖啡',
+    price: 120,
+    sku: '2L',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/6.jpg',
+  }, {
+    id: 7,
+    checked: false,
+    name: '果汁',
+    price: 125,
+    sku: '250ml',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/7.jpg',
+  }, {
+    id: 8,
+    checked: false,
+    name: '牛奶',
+    price: 130,
+    sku: '18.9L',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/8.jpg',
+  }, {
+    id: 9,
+    checked: false,
+    name: '酸奶',
+    price: 135,
+    sku: '箱装6瓶',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/9.jpg',
+  }, {
+    id: 10,
+    checked: false,
+    name: '豆浆',
+    price: 140,
+    sku: '箱装12瓶',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/10.jpg',
+  }, {
+    id: 11,
+    checked: false,
+    name: '可乐',
+    price: 145,
+    sku: '500ml+礼盒装',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/1.jpg',
+  }, {
+    id: 12,
+    checked: false,
+    name: '雪碧',
+    price: 150,
+    sku: '1L',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/2.jpg',
+  }, {
+    id: 13,
+    checked: false,
+    name: '矿泉水',
+    price: 155,
+    sku: '330ml',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/3.jpg',
+  }, {
+    id: 14,
+    checked: false,
+    name: '绿茶',
+    price: 160,
+    sku: '500ml+礼盒装',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/4.jpg',
+  }, {
+    id: 15,
+    checked: false,
+    name: '红茶',
+    price: 165,
+    sku: '1.5L',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/5.jpg',
+  }, {
+    id: 16,
+    checked: false,
+    name: '咖啡',
+    price: 170,
+    sku: '2L',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/6.jpg',
+  }, {
+    id: 17,
+    checked: false,
+    name: '果汁',
+    price: 175,
+    sku: '250ml',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/7.jpg',
+  }, {
+    id: 18,
+    checked: false,
+    name: '牛奶',
+    price: 180,
+    sku: '18.9L',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/8.jpg',
+  }, {
+    id: 19,
+    checked: false,
+    name: '酸奶',
+    price: 185,
+    sku: '箱装6瓶',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/9.jpg',
+  }, {
+    id: 20,
+    checked: false,
+    name: '豆浆',
+    price: 190,
+    sku: '箱装12瓶',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/10.jpg',
+  }, {
+    id: 21,
+    checked: false,
+    name: '可乐',
+    price: 195,
+    sku: '500ml+礼盒装',
+    amount: 1,
+    imgSrc: 'https://cdn.uviewui.com/uview/album/1.jpg',
+  }];
+};
+loadData();
 </script>
 
 <style lang="scss" scoped>
-  .xxx-content {
-    // padding: 0 16rpx;
-    max-height: calc(100vh - 102rpx);
+.good-price-label {
+  font-size: 16rpx;
+  line-height: 16rpx;
+  font-weight: 400;
+  color: $u-dbtj-label-price-color;
+}
 
-    .good-wrap {
-      padding: 0 16rpx;
-      margin-top: 20rpx;
-      padding-bottom: 180rpx; // 购物车结算栏 102rpx + margin-bottom 12rpx + 单行商品的一半 153/2 = 75+1.5 = 76.5rpx
+.xxx-content {
+  // padding: 0 16rpx;
+  max-height: calc(100vh - 102px);
 
-      .single-good {
-        margin-bottom: 12rpx;
-        padding-bottom: 20rpx;
-        width: 100%;
-        height: 153rpx;
-        background: #FFFFFF;
-        border-radius: 12rpx 12rpx 12rpx 12rpx;
+  .good-wrap {
+    padding: 0 16rpx;
+    margin-top: 20rpx;
+    padding-bottom: 180rpx; // 购物车结算栏 102rpx + margin-bottom 12rpx + 单行商品的一半 153/2 = 75+1.5 = 76.5rpx
+
+    .single-good {
+      margin-bottom: 20rpx;
+      padding-top: 20px;
+      padding-bottom: 20rpx;
+      display: flex;
+      align-items: center;
+      width: 100%;
+      height: 153rpx;
+      background: #ffffff;
+      border-radius: 12rpx 12rpx 12rpx 12rpx;
+
+      .single-select {
+        width: 70rpx;
+        display: flex;
+        padding-left: 26rpx;
+        flex: 10%;
+      }
+
+      .single-image {
+        flex: 20%;
+      }
+
+      .single-detail {
+        flex: 65%;
+        padding-left: 20rpx;
+        line-height: 113rpx;
+
+        .good-name {
+          height: 28rpx;
+          line-height: 28rpx;
+          font-weight: 400;
+          font-size: 28rpx;
+          color: $u-dbtj-label-main-color;
+        }
+
+        .good-sku {
+          height: 22rpx;
+          line-height: 22rpx;
+          font-weight: 400;
+          font-size: 22rpx;
+          color: $u-dbtj-label-right-order-color;
+          margin-top: 18rpx;
+          margin-bottom: 14rpx;
+        }
+
+        .good-price {
+          height: 32rpx;
+          line-height: 32rpx;
+          font-weight: 400;
+          font-size: 32rpx;
+          color: $u-dbtj-label-price-color;
+        }
+      }
+
+      .single-operation {
+        flex: 5%;
+        height: 113rpx;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        align-items: flex-end;
+        padding-right: 20rpx;
+
+        .delete {
+          width: 46rpx;
+          height: 22rpx;
+          line-height: 22rpx;
+          font-weight: 400;
+          font-size: 22rpx;
+          color: #999999;
+          color: $u-dbtj-label-right-order-color;
+        }
+
+        .amount {
+          text-align: center;
+
+          .amount-icon-view {
+            width: 20px;
+            text-align: center;
+            background: #f2f2f2;
+
+            .amount-icon {
+              text-align: center;
+              padding-left: 14rpx;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .collect-wrap {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 102rpx;
+    background: #fff;
+    border-radius: 12rpx 12rpx 0rpx 0rpx;
+    display: flex;
+    align-items: center;
+
+    .select-all {
+      flex: 40%;
+      display: flex;
+      align-items: center;
+      padding-left: 40rpx;
+      background: #fff;
+    }
+
+    .price-all {
+      flex: 30%;
+      padding-left: 20rpx;
+
+      .price-all-label {
+        width: 72rpx;
+        height: 30rpx;
+        line-height: 30rpx;
+        font-weight: 400;
+        font-size: 28rpx;
+        color: $u-dbtj-label-main-color;
+      }
+
+      .price-all-value {
+        padding-left: 10rpx;
+        width: 80rpx;
+        height: 32rpx;
+        font-weight: 400;
+        font-size: 32rpx;
+        color: #f80404;
       }
     }
 
-    .collect-wrap {
-      position: fixed;
-      bottom: 0;
-      width: 100%;
-      height: 102rpx;
-      background: #FFFF00;
-      border-radius: 12rpx 12rpx 0rpx 0rpx;
+    .handle-all {
+      flex: 30%;
+      padding-right: 20rpx;
+      color: $u-dbtj-label-main-color;
+      background-color: transparent;
+      color: transparent;
 
+      .handle-button {
+        width: 218rpx;
+        height: 62rpx;
+        background: #d5d5d5;
+        border-radius: 31rpx;
+      }
     }
   }
+}
+
+.u-checkbox__label-wrap {
+  text {
+    color: transparent;
+  }
+}
 </style>
 
 <style lang="scss">
-  page {
-    background-color: #efeff4;
-  }
+page {
+  background-color: #efeff4;
+}
 </style>
--- src/pages/tab/home/index.vue
+++ src/pages/tab/home/index.vue
@@ -3,16 +3,12 @@
     <up-gap bg-color="transparent" height="8" />
     <up-search bgColor="#FFF" v-model="keyword" placeholder="输入要搜索的商品名称" :action-style="actionStyle" />
     <up-gap bg-color="transparent" height="15" />
-    <up-swiper
-      class="swiper-wrap" :list="list2" key-name="image" :show-title="false" :autoplay="false" circular
-      @change="e => current = e.current"
-    >
+    <up-swiper class="swiper-wrap" :list="list2" key-name="image" :show-title="false" :autoplay="false" circular
+      @change="e => current = e.current">
       <template #indicator>
         <view class="indicator">
-          <view
-            v-for="(item, index) in list2" :key="index" class="indicator__dot"
-            :class="[index === current && 'indicator__dot--active']"
-          />
+          <view v-for="(item, index) in list2" :key="index" class="indicator__dot"
+            :class="[index === current && 'indicator__dot--active']" />
         </view>
       </template>
     </up-swiper>
@@ -88,182 +84,182 @@
 </template>
 
 <script setup>
-// 引入
-import {
-  onReachBottom,
-} from '@dcloudio/uni-app';
-import {
-  onMounted,
-  reactive,
-  ref,
-} from 'vue';
+  // 引入
+  import {
+    onReachBottom,
+  } from '@dcloudio/uni-app';
+  import {
+    onMounted,
+    reactive,
+    ref,
+  } from 'vue';
 
-// 响应式数据
-const keyword = ref('');
-const current = ref(0);
-const actionStyle = {
-  width: '112rpx',
-  height: ' 60rpx',
-  background: ' #2583B7',
-  borderRadius: '44rpx', // 圆角
-  textAlign: 'center', // 文本对齐方式
-  color: '#FFF',
-  lineHeight: '60rpx',
-};
+  // 响应式数据
+  const keyword = ref('');
+  const current = ref(0);
+  const actionStyle = {
+    width: '112rpx',
+    height: ' 60rpx',
+    background: ' #2583B7',
+    borderRadius: '44rpx', // 圆角
+    textAlign: 'center', // 文本对齐方式
+    color: '#FFF',
+    lineHeight: '60rpx',
+  };
   // 使用 reactive 创建响应式对象数组
-const list2 = reactive([{
-  image: 'https://cdn.uviewui.com/uview/swiper/swiper2.png',
-  title: '昨夜星辰昨夜风，画楼西畔桂堂东',
-}, {
-  image: 'https://cdn.uviewui.com/uview/swiper/swiper1.png',
-  title: '身无彩凤双飞翼，心有灵犀一点通',
-}, {
-  image: 'https://cdn.uviewui.com/uview/swiper/swiper3.png',
-  title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳',
-}]);
+  const list2 = reactive([{
+    image: 'https://cdn.uviewui.com/uview/swiper/swiper2.png',
+    title: '昨夜星辰昨夜风，画楼西畔桂堂东',
+  }, {
+    image: 'https://cdn.uviewui.com/uview/swiper/swiper1.png',
+    title: '身无彩凤双飞翼，心有灵犀一点通',
+  }, {
+    image: 'https://cdn.uviewui.com/uview/swiper/swiper3.png',
+    title: '谁念西风独自凉，萧萧黄叶闭疏窗，沉思往事立残阳',
+  }]);
 
-// 创建响应式数据
-const baseList = ref([{
-  name: 'photo',
-  title: '酒水',
-}, {
-  name: 'lock',
-  title: '文创',
-}, {
-  name: 'https://cdn.uviewui.com/uview/example/button.png',
-  title: '拓展',
-}]);
+  // 创建响应式数据
+  const baseList = ref([{
+    name: 'photo',
+    title: '酒水',
+  }, {
+    name: 'lock',
+    title: '文创',
+  }, {
+    name: 'https://cdn.uviewui.com/uview/example/button.png',
+    title: '拓展',
+  }]);
 
-// 创建对子组件的引用
-const uToastRef = ref(null);
-const loadStatus = ref('loadmore');
-const flowList = reactive([]);
-let goodList = reactive([]);
-const uWaterfallRef = ref();
+  // 创建对子组件的引用
+  const uToastRef = ref(null);
+  const loadStatus = ref('loadmore');
+  let flowList = reactive([]);
+  let goodList = reactive([]);
+  const uWaterfallRef = ref();
 
-// 定义方法
-const click = (name) => {
-  if (uToastRef.value) {
-    uToastRef.value.success(`点击了第${name}个`);
-  }
-};
+  // 定义方法
+  const click = (name) => {
+    if (uToastRef.value) {
+      uToastRef.value.success(`点击了第${name}个`);
+    }
+  };
 
-const setGoodList = () => {
-  goodList = [{
-    price: 35,
-    title: '北国风光，千里冰封，万里雪飘',
-    shop: '李白杜甫白居易旗舰店',
-    image: 'http://pic.sc.chinaz.com/Files/pic/pic9/202002/zzpic23327_s.jpg',
-    soldQuantity: 10,
-    sku: '500ml',
-  }, {
-    price: 75,
-    title: '望长城内外，惟余莽莽',
-    shop: '李白杜甫白居易旗舰店',
-    image: 'http://pic.sc.chinaz.com/Files/pic/pic9/202002/zzpic23325_s.jpg',
-    soldQuantity: 100,
-    sku: '100*50厘米',
-  }, {
-    price: 385,
-    title: '大河上下，顿失滔滔',
-    shop: '李白杜甫白居易旗舰店',
-    image: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg',
-    soldQuantity: 100,
-    sku: '1000ml',
-  }, {
-    price: 784,
-    title: '欲与天公试比高',
-    shop: '李白杜甫白居易旗舰店',
-    image: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/zzpic23369_s.jpg',
-    soldQuantity: 100,
-    sku: '200*50厘米',
-  }, {
-    price: 7891,
-    title: '须晴日，看红装素裹，分外妖娆',
-    shop: '李白杜甫白居易旗舰店',
-    image: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2130_s.jpg',
-    soldQuantity: 100,
-    sku: '150*50厘米',
-  }, {
-    price: 2341,
-    shop: '李白杜甫白居易旗舰店',
-    title: '江山如此多娇，引无数英雄竞折腰',
-    image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23346_s.jpg',
-    soldQuantity: 100,
-    sku: '250*50厘米',
-  }, {
-    price: 661,
-    shop: '李白杜甫白居易旗舰店',
-    title: '惜秦皇汉武，略输文采',
-    image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23344_s.jpg',
-    soldQuantity: 100,
-    sku: '100*150厘米',
-  }, {
-    price: 1654,
-    title: '唐宗宋祖，稍逊风骚',
-    shop: '李白杜甫白居易旗舰店',
-    image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg',
-    soldQuantity: 80,
-    sku: '1500ml',
-  }, {
-    price: 1678,
-    title: '一代天骄，成吉思汗',
-    shop: '李白杜甫白居易旗舰店',
-    image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg',
-    soldQuantity: 50,
-    sku: '2500ml',
-  }, {
-    price: 924,
-    title: '只识弯弓射大雕',
-    shop: '李白杜甫白居易旗舰店',
-    image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg',
-    soldQuantity: 100,
-    sku: '1500ml',
-  }, {
-    price: 8243,
-    title: '俱往矣，数风流人物，还看今朝',
-    shop: '李白杜甫白居易旗舰店',
-    image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg',
-    soldQuantity: 60,
-    sku: '500ml',
-  }];
+  const setGoodList = () => {
+    goodList = [{
+      price: 35,
+      title: '北国风光，千里冰封，万里雪飘',
+      shop: '李白杜甫白居易旗舰店',
+      image: 'http://pic.sc.chinaz.com/Files/pic/pic9/202002/zzpic23327_s.jpg',
+      soldQuantity: 10,
+      sku: '500ml',
+    }, {
+      price: 75,
+      title: '望长城内外，惟余莽莽',
+      shop: '李白杜甫白居易旗舰店',
+      image: 'http://pic.sc.chinaz.com/Files/pic/pic9/202002/zzpic23325_s.jpg',
+      soldQuantity: 100,
+      sku: '100*50厘米',
+    }, {
+      price: 385,
+      title: '大河上下，顿失滔滔',
+      shop: '李白杜甫白居易旗舰店',
+      image: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg',
+      soldQuantity: 100,
+      sku: '1000ml',
+    }, {
+      price: 784,
+      title: '欲与天公试比高',
+      shop: '李白杜甫白居易旗舰店',
+      image: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/zzpic23369_s.jpg',
+      soldQuantity: 100,
+      sku: '200*50厘米',
+    }, {
+      price: 7891,
+      title: '须晴日，看红装素裹，分外妖娆',
+      shop: '李白杜甫白居易旗舰店',
+      image: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2130_s.jpg',
+      soldQuantity: 100,
+      sku: '150*50厘米',
+    }, {
+      price: 2341,
+      shop: '李白杜甫白居易旗舰店',
+      title: '江山如此多娇，引无数英雄竞折腰',
+      image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23346_s.jpg',
+      soldQuantity: 100,
+      sku: '250*50厘米',
+    }, {
+      price: 661,
+      shop: '李白杜甫白居易旗舰店',
+      title: '惜秦皇汉武，略输文采',
+      image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23344_s.jpg',
+      soldQuantity: 100,
+      sku: '100*150厘米',
+    }, {
+      price: 1654,
+      title: '唐宗宋祖，稍逊风骚',
+      shop: '李白杜甫白居易旗舰店',
+      image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg',
+      soldQuantity: 80,
+      sku: '1500ml',
+    }, {
+      price: 1678,
+      title: '一代天骄，成吉思汗',
+      shop: '李白杜甫白居易旗舰店',
+      image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg',
+      soldQuantity: 50,
+      sku: '2500ml',
+    }, {
+      price: 924,
+      title: '只识弯弓射大雕',
+      shop: '李白杜甫白居易旗舰店',
+      image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg',
+      soldQuantity: 100,
+      sku: '1500ml',
+    }, {
+      price: 8243,
+      title: '俱往矣，数风流人物，还看今朝',
+      shop: '李白杜甫白居易旗舰店',
+      image: 'http://pic1.sc.chinaz.com/Files/pic/pic9/202002/zzpic23343_s.jpg',
+      soldQuantity: 60,
+      sku: '500ml',
+    }];
 
-  console.log(goodList, 'goodList');
-};
+    console.log(goodList, 'goodList');
+  };
   // const remove = (id) => {
   //   uWaterfallRef.value.remove(id);
   // };
-const clear = () => {
-  uWaterfallRef.value.clear();
-};
+  const clear = () => {
+    uWaterfallRef.value.clear();
+  };
 
-const addRandomData = () => {
-  console.log(uni.$u.config.v, '********');
-  console.log(uni.$u.guid(), '********');
-  console.log(uni.$u.random(0, 9), '********');
+  const addRandomData = () => {
+    console.log(uni.$u.config.v, '********');
+    console.log(uni.$u.guid(), '********');
+    console.log(uni.$u.random(0, 9), '********');
 
-  for (let i = 0; i < goodList.length; i++) {
-    const index = uni.$u.random(0, goodList.length - 1);
-    // 先转成字符串再转成对象，避免数组对象引用导致数据混乱
-    const item = JSON.parse(JSON.stringify(goodList[index]));
-    item.id = uni.$u.guid();
-    flowList.push(item);
-  }
-};
+    for (let i = 0; i < goodList.length; i++) {
+      const index = uni.$u.random(0, goodList.length - 1);
+      // 先转成字符串再转成对象，避免数组对象引用导致数据混乱
+      const item = JSON.parse(JSON.stringify(goodList[index]));
+      item.id = uni.$u.guid();
+      flowList.push(item);
+    }
+  };
 
-onReachBottom(() => {
-  loadStatus.value = 'loading';
-  // 模拟数据加载
-  setTimeout(() => {
+  onReachBottom(() => {
+    loadStatus.value = 'loading';
+    // 模拟数据加载
+    setTimeout(() => {
+      addRandomData();
+      loadStatus.value = 'loadmore';
+    }, 1000);
+  });
+
+  onMounted(() => {
+    setGoodList();
     addRandomData();
-    loadStatus.value = 'loadmore';
-  }, 1000);
-});
-
-onMounted(() => {
-  setGoodList();
-  addRandomData();
-});
+  });
 </script>
 
 <style lang="scss">
@@ -350,17 +346,17 @@ onMounted(() => {
   }
 
   .blobk-name {
-      margin-top: 17rpx;
-      margin-bottom: 13rpx;
-      width: 96rpx;
-      height: 45rpx;
-      font-weight: 400;
-      font-size: 32rpx;
-      color: #333333;
-      line-height: 45rpx;
-      text-align: left;
-      font-style: normal;
-      text-transform: none;
+    margin-top: 17rpx;
+    margin-bottom: 13rpx;
+    width: 96rpx;
+    height: 45rpx;
+    font-weight: 400;
+    font-size: 32rpx;
+    color: #333333;
+    line-height: 45rpx;
+    text-align: left;
+    font-style: normal;
+    text-transform: none;
   }
 
   .demo-image {
