6373cd311b6327cc352e0d9bf4b8f3707cc45e72
jiang yadong <18827637106@163.com>
2024-12-17 16:57:01 +0800

    feat:订单确认界面

--- src/pages/orderMana/orderConfirm/index.vue
+++ src/pages/orderMana/orderConfirm/index.vue
@@ -2,12 +2,118 @@
   <view class="order-confirm-content">
     <view class="content-wrap">
       <view class="address-wrap">
+        <view v-if="buyMode === 'direct'" class="address-info">
+          <view class="address-name u-line-2">
+            湖北省武汉市洪山区关东街道XXXXXXXXXXXXXXXXXXXXXXX
+          </view>
+          <view class="people-info">
+            <view class="people-info-name">王先生</view>
+            <view class="people-info-phone">18812345678</view>
+          </view>
+        </view>
+        <view v-else class="address-info">
+          <view class="address-name u-line-2">
+            收货地址由好友填写
+          </view>
+          <view class="people-info">
+            <view class="people-info-name"></view>
+            <view class="people-info-phone">将生成的提货码以链接的形式分享给微信好友，由好友填写收货地址</view>
+          </view>
+        </view>
 
       </view>
       <view class="good-wrap">
+        <view v-for="(item,index) in baseketList" :key="item.id" class="single-good">
+          <view class="single-image">
+            <up-image bg-color="#fff" class="image" :show-loading="true" :src="item.imgSrc" width="113rpx"
+              height="113rpx" />
+          </view>
+          <view class="single-detail">
+            <view class="good-name up-line-1">
+              {{ item.name }}
+            </view>
+            <view class="good-sku up-line-1">
+              {{ item.sku }}
+            </view>
+            <view class="good-tag">
+              <text class="good-tag-label">{{ "七天无理由退货" }}</text>
+            </view>
+          </view>
+          <view class="single-operation">
 
+            <view class="good-price up-line-1">
+              <text class="good-price-label">
+                ¥
+              </text> {{ item.price }}
+            </view>
+            <view class="amount">
+              <up-number-box :disabled="true" v-model="item.amount" integer button-size="5" color="#ffffff"
+                bg-color="#f2f2f2" icon-style="color: #fff" :min="1" :max="100">
+                <template #minus>
+                  <view class="minus amount-icon-view">
+                    <up-icon class="amount-icon" name="minus" size="5" />
+                  </view>
+                </template>
+                <template #input>
+                  <text style="
+                    width: 20px;
+                    text-align: center;
+                    color: #333;
+                    font-size: 19rpx;
+                  " class="input">
+                    {{ item.amount }}
+                  </text>
+                </template>
+                <template #plus>
+                  <view class="plus amount-icon-view">
+                    <up-icon class="amount-icon" name="plus" size="5" />
+                  </view>
+                </template>
+              </up-number-box>
+            </view>
+          </view>
+        </view>
+        <up-cell-group :border="false">
+          <up-cell style="padding:0" :border="false" size="small" title="配送服务" value="快递 包邮"></up-cell>
+          <up-cell style="padding:0" :border="false" size="small" title="订单备注" value="周末送货上门" isLink></up-cell>
+        </up-cell-group>
       </view>
-      <view class="pay-wrap"></view>
+      <view class="pay-wrap">
+        <view class="pay-wrap-label">支付方式</view>
+        <up-cell-group>
+          <up-cell title="微信支付">
+            <template #icon>
+              <up-icon size="24" name="weixin-fill" color="#07c160"></up-icon>
+            </template>
+            <!-- <up-badge count="99" :absolute="false" slot="right-icon"></up-badge> -->
+            <template #right-icon>
+              <up-checkbox :disabled="true" label="" v-model:checked="isWeixinPay" :custom-style="{}" used-alone
+                shape="circle" active-color="#07c160" bg-color="#fff" @change="weixinPayChange" />
+            </template>
+          </up-cell>
+        </up-cell-group>
+      </view>
+    </view>
+
+    <view class="collect-wrap">
+      <view class="select-all">
+        <!--        <up-checkbox label="全选" v-model:checked="isAllChecked" :custom-style="{}" used-alone shape="circle"
+          active-color="#07c160" bg-color="#fff" /> -->
+      </view>
+      <view class="price-all">
+        <text class="price-all-label">
+          合计:
+        </text>
+        <text class="good-price-label">
+          ¥
+        </text>
+        <text class="price-all-value">
+          {{ priceTotal }}
+        </text>
+      </view>
+      <view class="handle-all">
+        <up-button style="color: #fff" class="handle-button" color="#ff6600" shape="circle" text="去支付" />
+      </view>
     </view>
   </view>
 </template>
@@ -16,15 +122,66 @@
   import {
     onLoad
   } from '@dcloudio/uni-app';
-  let buyMode = ref();
-
+  let buyMode = ref('');
+  let isWeixinPay = ref(true);
+  let priceTotal = ref(600);
   onLoad((options) => {
     console.log(options.buyMode, "options");
     buyMode.value = options.buyMode;
   })
+  const baseketList = ref([]);
+  const loadData = () => {
+    baseketList.value = [{
+        id: 1,
+        checked: false,
+        name: '可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐',
+        price: 100,
+        sku: '500ml+礼盒装',
+        amount: 1,
+        imgSrc: 'https://cdn.uviewui.com/uview/album/1.jpg',
+      }, {
+        id: 2,
+        checked: false,
+        name: '雪碧',
+        price: 105,
+        sku: '1L',
+        amount: 1,
+        imgSrc: 'https://cdn.uviewui.com/uview/album/2.jpg',
+      },
+      {
+        id: 1,
+        checked: false,
+        name: '可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐',
+        price: 100,
+        sku: '500ml+礼盒装',
+        amount: 1,
+        imgSrc: 'https://cdn.uviewui.com/uview/album/1.jpg',
+      }
+    ];
+  };
+  loadData();
+
+  const weixinPayChange = (value) => {
+    isWeixinPay.value = true;
+  }
 </script>
 
 <style lang="scss" scoped>
+  .good-price {
+    height: 32rpx;
+    line-height: 32rpx;
+    font-weight: 400;
+    font-size: 32rpx;
+    color: $u-dbtj-label-price-color;
+  }
+
+  .good-price-label {
+    font-size: 16rpx;
+    line-height: 16rpx;
+    font-weight: 400;
+    color: $u-dbtj-label-price-color;
+  }
+
   .order-confirm-content {
     // padding: 0 16rpx;
     max-height: calc(100vh - 102px);
@@ -32,20 +189,285 @@
     .content-wrap {
       padding: 0 16rpx;
       margin-top: 20rpx;
-      background-color: red;
       max-height: 100vh;
+      padding-bottom: 200px;
 
       .address-wrap {
         width: 718rpx;
         height: 129rpx;
         background: #FFFFFF;
         border-radius: 12rpx 12rpx 12rpx 12rpx;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+
+        .address-info {
+          padding-left: 10rpx;
+          flex: 71.33%;
+          color: $u-dbtj-label-main-color;
+          font-weight: 400;
+          text-align: left;
+          font-style: normal;
+          text-transform: none;
+
+          .address-name {
+            padding-left: 10rpx;
+            font-size: 28rpx;
+            line-height: 40rpx;
+            text-align: left;
+            font-style: normal;
+            text-transform: none;
+          }
+
+          .people-info {
+            padding-left: 10rpx;
+            display: flex;
+            align-items: center;
+
+            .people-info-name {
+
+              height: 30rpx;
+              font-size: 22rpx;
+              line-height: 30rpx;
+
+            }
+
+            .people-info-phone {
+              height: 30rpx;
+              font-size: 22rpx;
+              color: $u-dbtj-label-right-order-color;
+              line-height: 33rpx;
+
+            }
+          }
+        }
+
+        .address-icon {
+          flex: 28.67%;
+          display: flex;
+          justify-content: flex-end;
+          padding-right: 20rpx;
+        }
+
       }
 
-      .good-wrap {}
 
-      .pay-wrap {}
+
+      .good-wrap {
+        padding: 0 16rpx;
+        margin-top: 20rpx;
+        background: #fff;
+
+        .single-good {
+
+
+          margin-bottom: 20rpx;
+          display: flex;
+          align-items: center;
+          width: 100%;
+          height: 153rpx;
+          background: #ffffff;
+          border-radius: 12rpx 12rpx 12rpx 12rpx;
+
+          .single-select {
+            width: 70rpx;
+            display: flex;
+            padding-left: 26rpx;
+            flex: 10%;
+          }
+
+          .single-image {
+            flex: 20%;
+            background-color: transparent;
+            padding-left: 10rpx;
+          }
+
+          .single-detail {
+            flex: 65%;
+            padding-left: 20rpx;
+            line-height: 113rpx;
+
+            .good-name {
+              height: 28rpx;
+              line-height: 28rpx;
+              font-weight: 400;
+              font-size: 28rpx;
+              color: $u-dbtj-label-main-color;
+            }
+
+            .good-sku {
+              height: 22rpx;
+              line-height: 22rpx;
+              font-weight: 400;
+              font-size: 22rpx;
+              color: $u-dbtj-label-right-order-color;
+              margin-top: 18rpx;
+              margin-bottom: 14rpx;
+            }
+
+            .good-price {
+              height: 32rpx;
+              line-height: 32rpx;
+              font-weight: 400;
+              font-size: 32rpx;
+              color: $u-dbtj-label-price-color;
+            }
+
+            .good-tag {
+
+              height: 22rpx;
+              line-height: 22rpx;
+
+              .good-tag-label {
+                padding-left: 10rpx;
+                padding-right: 10rpx;
+                height: 22rpx;
+                line-height: 22rpx;
+                font-weight: 400;
+                font-size: 16rpx;
+                color: $u-dbtj-label-price-color;
+                border: 1rpx solid $u-dbtj-label-price-color;
+              }
+            }
+          }
+
+          .single-operation {
+            flex: 5%;
+            height: 113rpx;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: flex-end;
+
+            .delete {
+              width: 46rpx;
+              height: 22rpx;
+              line-height: 22rpx;
+              font-weight: 400;
+              font-size: 22rpx;
+              color: #999999;
+              color: $u-dbtj-label-right-order-color;
+            }
+
+            .amount {
+              text-align: center;
+
+              .amount-icon-view {
+                width: 20px;
+                text-align: center;
+                background: #f2f2f2;
+
+                .amount-icon {
+                  text-align: center;
+                  padding-left: 14rpx;
+                }
+              }
+            }
+          }
+        }
+
+
+      }
+
+
+
+      .pay-wrap {
+        margin-top: 20rpx;
+        padding: 20rpx;
+        min-height: 149rpx;
+        background: #FFFFFF;
+        border-radius: 12rpx 12rpx 12rpx 12rpx;
+
+        .pay-wrap-label {
+          padding-top: 14rpx;
+          padding-bottom: 14rpx;
+          width: 112rpx;
+          height: 40rpx;
+          font-size: 28rpx;
+          color: $u-dbtj-label-main-color;
+          line-height: 40rpx;
+          text-align: left;
+          font-style: normal;
+          text-transform: none;
+        }
+
+      }
     }
+
+
+    .collect-wrap {
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      height: 102rpx;
+      background: #fff;
+      border-radius: 12rpx 12rpx 0rpx 0rpx;
+      display: flex;
+      align-items: center;
+
+      .select-all {
+        display: flex;
+        padding-left: 26rpx;
+        flex: 40%;
+
+      }
+
+      .price-all {
+        flex: 30%;
+        padding-left: 20rpx;
+
+        .price-all-label {
+          width: 72rpx;
+          height: 30rpx;
+          line-height: 30rpx;
+          font-weight: 400;
+          font-size: 28rpx;
+          color: $u-dbtj-label-main-color;
+        }
+
+        .price-all-value {
+          padding-left: 10rpx;
+          width: 80rpx;
+          height: 32rpx;
+          font-weight: 400;
+          font-size: 32rpx;
+          color: #f80404;
+        }
+      }
+
+      .handle-all {
+        flex: 30%;
+        padding-right: 20rpx;
+        color: $u-dbtj-label-main-color;
+        background-color: transparent;
+        color: transparent;
+
+        .handle-button {
+          width: 218rpx;
+          height: 62rpx;
+          background: #d5d5d5;
+          border-radius: 31rpx;
+        }
+      }
+    }
+  }
+
+
+  ::v-deep .u-cell-group .u-cell__body {
+    font-weight: 400;
+    font-size: 24rpx;
+    height: 33rpx;
+    color: $u-dbtj-label-main-color;
+    line-height: 33rpx;
+    text-align: left;
+    font-style: normal;
+    text-transform: none;
+    padding: 35rpx 0 !important;
+  }
+
+  ::v-deep .u-cell-group .u-cell__left-icon-wrap {
+    width: 60rpx;
   }
 </style>
 
---
+++ src/pages/tab/basket/basket.vue
@@ -0,0 +1,573 @@
+<template>
+  <view class="xxx-content">
+    <view class="good-wrap">
+      <view v-for="(item,index) in baseketList" :key="item.id" class="single-good">
+        <view class="single-select">
+          <up-checkbox v-model:checked="item.checked" :custom-style="{}" used-alone shape="circle"
+            active-color="#07c160" bg-color="#fff" />
+        </view>
+        <view class="single-image">
+          <up-image bg-color="#fff" class="image" :show-loading="true" :src="item.imgSrc" width="113rpx"
+            height="113rpx" />
+        </view>
+        <view class="single-detail">
+          <view class="good-name up-line-1">
+            {{ item.name }}
+          </view>
+          <view class="good-sku up-line-1">
+            {{ item.sku }}
+          </view>
+          <view class="good-price up-line-1">
+            <text class="good-price-label">
+              ¥
+            </text> {{ item.price }}
+          </view>
+        </view>
+        <view class="single-operation">
+
+          <view class="delete" @click="showDeleteModal(item,index)">
+            移除
+          </view>
+          <view class="amount">
+            <up-number-box v-model="item.amount" integer button-size="5" color="#ffffff" bg-color="#f2f2f2"
+              icon-style="color: #fff" :min="1" :max="100">
+              <template #minus>
+                <view class="minus amount-icon-view">
+                  <up-icon class="amount-icon" name="minus" size="5" />
+                </view>
+              </template>
+              <template #input>
+                <text style="
+                    width: 20px;
+                    text-align: center;
+                    color: #333;
+                    font-size: 19rpx;
+                  " class="input">
+                  {{ item.amount }}
+                </text>
+              </template>
+              <template #plus>
+                <view class="plus amount-icon-view">
+                  <up-icon class="amount-icon" name="plus" size="5" />
+                </view>
+              </template>
+            </up-number-box>
+          </view>
+        </view>
+      </view>
+    </view>
+
+    <view class="collect-wrap">
+      <view class="select-all">
+        <up-checkbox label="全选" v-model:checked="isAllChecked" :custom-style="{}" used-alone shape="circle"
+          active-color="#07c160" bg-color="#fff" />
+      </view>
+      <view class="price-all">
+        <text class="price-all-label">
+          合计:
+        </text>
+        <text class="good-price-label">
+          ¥
+        </text>
+        <text class="price-all-value">
+          {{ priceTotal }}
+        </text>
+      </view>
+      <view class="handle-all">
+        <up-button @click="showBuyModeModal" style="color: #333" class="handle-button" color="#d5d5d5" shape="circle"
+          text="去结算" />
+      </view>
+    </view>
+  </view>
+  <!-- 移出购物车商品弹窗 -->
+  <view class="">
+    <up-modal :title="deleteModalTitle" :show="deleteModalShow" @confirm="deleteModalconfirm"
+      @cancel="deleteModalCancel" ref="deleteModalRef" :asyncClose="false" :showCancelButton="true"
+      :showConfirmButton="true" contentTextAlign="center"></up-modal>
+  </view>
+  <!-- 结算弹窗 -->
+  <view class="">
+    <up-modal :title="buyModeModalTitle" :show="buyModeModalShow" ref="buyModeModalRef" :asyncClose="false"
+      :showCancelButton="false" :showConfirmButton="false" contentTextAlign="center" :closeOnClickOverlay="true">
+      <view class="mode-button-wrap">
+        <up-button @click="buyModeDirectConfirm" style="color: #333" class="handle-button" color="#d5d5d5"
+          shape="circle" text="直接购买" />
+        <up-button @click="buyModeGiveConfirm" style="color: #333" class="handle-button mt-20rpx" color="#d5d5d5"
+          shape="circle" text="赠送好友" />
+      </view>
+
+    </up-modal>
+  </view>
+</template>
+
+<script setup lang="js">
+  import {
+    reactive,
+    ref,
+  } from 'vue';
+  import {
+    onLoad,
+    onShow
+  } from "@dcloudio/uni-app";
+  import {
+    queryParams
+  } from 'uview-plus';
+  const deleteModalRef = ref();
+  const deleteModalTitle = ref('确认要删除商品么?');
+  let deleteIndex = ref();
+  // 创建响应式数据
+  let deleteModalShow = ref(false);
+
+  // 方法
+  const showDeleteModal = (item, index) => {
+    deleteModalShow.value = true;
+    deleteIndex.value = index;
+
+  };
+
+  const deleteModalconfirm = () => {
+    baseketList.value.splice(deleteIndex.value, 1);
+    deleteModalShow.value = false;
+  };
+
+  const deleteModalCancel = () => {
+    deleteModalShow.value = false;
+  };
+
+  let PurchaseType = {
+    DirectPurchase: 'direct',
+    GiftToFriend: 'indirect'
+  }
+  const buyModeModalRef = ref();
+  const buyModeModalTitle = ref('请选择购买形式?');
+  let buyMode = ref();
+  // 创建响应式数据
+  let buyModeModalShow = ref(false);
+
+  // 方法
+  const showBuyModeModal = (item, index) => {
+    buyModeModalShow.value = true;
+
+  };
+
+  // TODO:写一个函数
+  /*
+   * @description 写一个函数跳转并且传参
+   * @param {number} buyMode
+   * @param {string} basketCheckedObj
+   *
+   */
+
+  const buyModeDirectConfirm = () => {
+    buyMode.value = PurchaseType.DirectPurchase;
+    console.log(buyMode.value, "buyMode.value");
+    buyModeModalShow.value = false;
+    let params = {
+      buyMode: buyMode.value
+    };
+    let queryString = queryParams(params);
+    console.log(queryString, "queryString");
+    uni.navigateTo({
+      url: '/pages/orderMana/orderConfirm/index' + queryString,
+    })
+  };
+
+  const buyModeGiveConfirm = () => {
+    buyMode.value = PurchaseType.GiftToFriend;
+    console.log(buyMode.value, "buyMode.value");
+    buyModeModalShow.value = false;
+    let params = {
+      buyMode: buyMode.value
+    };
+    let queryString = queryParams(params);
+    console.log(queryString, "queryString");
+    uni.navigateTo({
+      url: '/pages/orderMana/orderConfirm/index' + queryString,
+    })
+  };
+
+
+
+
+
+  // uni-app 的生命周期钩子
+  onLoad((opt) => {
+    // 页面加载时执行的代码
+    console.log('页面加载了', opt);
+  });
+
+  onShow(() => {
+    // 页面显示时执行的代码
+    console.log('页面显示了');
+  });
+
+  const baseketList = ref([]);
+  const priceTotal = ref(600);
+  const isAllChecked = ref(false);
+  const actionStyle = {
+    display: 'flex',
+    flex: '40%',
+    paddingLeft: ' 26rpx',
+  };
+  const loadData = () => {
+    baseketList.value = [{
+      id: 1,
+      checked: false,
+      name: '可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐可乐',
+      price: 100,
+      sku: '500ml+礼盒装',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/1.jpg',
+    }, {
+      id: 2,
+      checked: false,
+      name: '雪碧',
+      price: 105,
+      sku: '1L',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/2.jpg',
+    }, {
+      id: 3,
+      checked: false,
+      name: '矿泉水',
+      price: 95,
+      sku: '330ml',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/3.jpg',
+    }, {
+      id: 4,
+      checked: false,
+      name: '绿茶',
+      price: 110,
+      sku: '500ml+礼盒装',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/4.jpg',
+    }, {
+      id: 5,
+      checked: false,
+      name: '红茶',
+      price: 115,
+      sku: '1.5L',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/5.jpg',
+    }, {
+      id: 6,
+      checked: false,
+      name: '咖啡',
+      price: 120,
+      sku: '2L',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/6.jpg',
+    }, {
+      id: 7,
+      checked: false,
+      name: '果汁',
+      price: 125,
+      sku: '250ml',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/7.jpg',
+    }, {
+      id: 8,
+      checked: false,
+      name: '牛奶',
+      price: 130,
+      sku: '18.9L',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/8.jpg',
+    }, {
+      id: 9,
+      checked: false,
+      name: '酸奶',
+      price: 135,
+      sku: '箱装6瓶',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/9.jpg',
+    }, {
+      id: 10,
+      checked: false,
+      name: '豆浆',
+      price: 140,
+      sku: '箱装12瓶',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/10.jpg',
+    }, {
+      id: 11,
+      checked: false,
+      name: '可乐',
+      price: 145,
+      sku: '500ml+礼盒装',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/1.jpg',
+    }, {
+      id: 12,
+      checked: false,
+      name: '雪碧',
+      price: 150,
+      sku: '1L',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/2.jpg',
+    }, {
+      id: 13,
+      checked: false,
+      name: '矿泉水',
+      price: 155,
+      sku: '330ml',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/3.jpg',
+    }, {
+      id: 14,
+      checked: false,
+      name: '绿茶',
+      price: 160,
+      sku: '500ml+礼盒装',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/4.jpg',
+    }, {
+      id: 15,
+      checked: false,
+      name: '红茶',
+      price: 165,
+      sku: '1.5L',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/5.jpg',
+    }, {
+      id: 16,
+      checked: false,
+      name: '咖啡',
+      price: 170,
+      sku: '2L',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/6.jpg',
+    }, {
+      id: 17,
+      checked: false,
+      name: '果汁',
+      price: 175,
+      sku: '250ml',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/7.jpg',
+    }, {
+      id: 18,
+      checked: false,
+      name: '牛奶',
+      price: 180,
+      sku: '18.9L',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/8.jpg',
+    }, {
+      id: 19,
+      checked: false,
+      name: '酸奶',
+      price: 185,
+      sku: '箱装6瓶',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/9.jpg',
+    }, {
+      id: 20,
+      checked: false,
+      name: '豆浆',
+      price: 190,
+      sku: '箱装12瓶',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/10.jpg',
+    }, {
+      id: 21,
+      checked: false,
+      name: '可乐',
+      price: 195,
+      sku: '500ml+礼盒装',
+      amount: 1,
+      imgSrc: 'https://cdn.uviewui.com/uview/album/1.jpg',
+    }];
+  };
+  loadData();
+</script>
+
+<style lang="scss" scoped>
+  .good-price-label {
+    font-size: 16rpx;
+    line-height: 16rpx;
+    font-weight: 400;
+    color: $u-dbtj-label-price-color;
+  }
+
+  .xxx-content {
+    // padding: 0 16rpx;
+    max-height: calc(100vh - 102px);
+
+    .good-wrap {
+      padding: 0 16rpx;
+      margin-top: 20rpx;
+      padding-bottom: 180rpx; // 购物车结算栏 102rpx + margin-bottom 12rpx + 单行商品的一半 153/2 = 75+1.5 = 76.5rpx
+
+      .single-good {
+        margin-bottom: 20rpx;
+        display: flex;
+        align-items: center;
+        width: 100%;
+        height: 153rpx;
+        background: #ffffff;
+        border-radius: 12rpx 12rpx 12rpx 12rpx;
+
+        .single-select {
+          width: 70rpx;
+          display: flex;
+          padding-left: 26rpx;
+          flex: 10%;
+        }
+
+        .single-image {
+          flex: 20%;
+          background-color: transparent
+        }
+
+        .single-detail {
+          flex: 65%;
+          padding-left: 20rpx;
+          line-height: 113rpx;
+
+          .good-name {
+            height: 28rpx;
+            line-height: 28rpx;
+            font-weight: 400;
+            font-size: 28rpx;
+            color: $u-dbtj-label-main-color;
+          }
+
+          .good-sku {
+            height: 22rpx;
+            line-height: 22rpx;
+            font-weight: 400;
+            font-size: 22rpx;
+            color: $u-dbtj-label-right-order-color;
+            margin-top: 18rpx;
+            margin-bottom: 14rpx;
+          }
+
+          .good-price {
+            height: 32rpx;
+            line-height: 32rpx;
+            font-weight: 400;
+            font-size: 32rpx;
+            color: $u-dbtj-label-price-color;
+          }
+        }
+
+        .single-operation {
+          flex: 5%;
+          height: 113rpx;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: flex-end;
+          padding-right: 20rpx;
+
+          .delete {
+            width: 46rpx;
+            height: 22rpx;
+            line-height: 22rpx;
+            font-weight: 400;
+            font-size: 22rpx;
+            color: #999999;
+            color: $u-dbtj-label-right-order-color;
+          }
+
+          .amount {
+            text-align: center;
+
+            .amount-icon-view {
+              width: 20px;
+              text-align: center;
+              background: #f2f2f2;
+
+              .amount-icon {
+                text-align: center;
+                padding-left: 14rpx;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    .collect-wrap {
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      height: 102rpx;
+      background: #fff;
+      border-radius: 12rpx 12rpx 0rpx 0rpx;
+      display: flex;
+      align-items: center;
+
+      .select-all {
+        display: flex;
+        padding-left: 26rpx;
+        flex: 40%;
+
+      }
+
+      .price-all {
+        flex: 30%;
+        padding-left: 20rpx;
+
+        .price-all-label {
+          width: 72rpx;
+          height: 30rpx;
+          line-height: 30rpx;
+          font-weight: 400;
+          font-size: 28rpx;
+          color: $u-dbtj-label-main-color;
+        }
+
+        .price-all-value {
+          padding-left: 10rpx;
+          width: 80rpx;
+          height: 32rpx;
+          font-weight: 400;
+          font-size: 32rpx;
+          color: #f80404;
+        }
+      }
+
+      .handle-all {
+        flex: 30%;
+        padding-right: 20rpx;
+        color: $u-dbtj-label-main-color;
+        background-color: transparent;
+        color: transparent;
+
+        .handle-button {
+          width: 218rpx;
+          height: 62rpx;
+          background: #d5d5d5;
+          border-radius: 31rpx;
+        }
+      }
+    }
+  }
+
+  .u-checkbox__label-wrap {
+    text {
+      color: #f2f2f2;
+    }
+  }
+
+  .mode-button-wrap {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    .handle-button {
+      height: 62rpx;
+      background: #d5d5d5;
+      border-radius: 31rpx;
+    }
+  }
+</style>
+
+<style lang="scss">
+  page {
+    background-color: #efeff4;
+  }
+</style>
