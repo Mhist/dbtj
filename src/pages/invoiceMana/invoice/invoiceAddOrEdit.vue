d9ebbbbc1db06560a8da04a33b51f7cddfe98daf
jiang yadong <18827637106@163.com>
2024-12-20 11:55:43 +0800

    feat:发票管理

--- src/pages/invoiceMana/invoice/invoiceAddOrEdit.vue
+++ src/pages/invoiceMana/invoice/invoiceAddOrEdit.vue
@@ -2,42 +2,44 @@
   <view class="wrap">
     <u-form class="form-wrap" ref="form" :model="formData">
       <u-form-item labelWidth="120" label="抬头类型" prop="headerType">
-        <u-radio-group v-model="formData.headerType">
-          <u-radio label="个人/非企业单位" :name="formData.headerType"></u-radio>
-          <u-radio label="企业" :name="formData.headerType"></u-radio>
+        <u-radio-group v-model="formData.headerType" @change="groupChange">
+          <up-radio :customStyle="{marginBottom: '8px'}" v-for="(item, index) in radiolist" :key="index"
+            :label="item.name" :name="item.name" @change="radioChange">
+          </up-radio>
         </u-radio-group>
       </u-form-item>
-      <u-form-item labelWidth="120" label="发票抬头" prop="invoiceHeader" :rules="[{ required: true, message: '请填写抬头名称' }]">
+      <u-form-item labelWidth="120" label="发票抬头" prop="invoiceHeader"
+        :rules="[{ required: true, message: '请填写抬头名称',trigger: ['blur'] }]">
         <u-input v-model="formData.invoiceHeader" placeholder="请填写抬头名称(必填)"></u-input>
       </u-form-item>
-      <u-form-item labelWidth="120" label="纳税人识别号" prop="taxpayerId" placeholder="请填写纳税人识别号(必填)"
-        :rules="[{ required: true, message: '请填写纳税人识别号' }]">
-        <u-input v-model="formData.taxpayerId"></u-input>
+      <u-form-item v-if="showTaxPayer" labelWidth="120" label="纳税人识别号" prop="taxpayerId"
+        :rules="[{ required: true, message: '请填写纳税人识别号',trigger: ['blur'] }]">
+        <u-input placeholder="请填写纳税人识别号(必填)" v-model="formData.taxpayerId"></u-input>
       </u-form-item>
-      <u-form-item labelWidth="120" label="邮箱地址" prop="email" placeholder="用于接收电子发票(必填)" :rules=" [{ required: true,
-        message: '用户接收电子发票' }]">
-        <u-input v-model="formData.email"></u-input>
+      <u-form-item labelWidth="120" label="邮箱地址" prop="email" :rules=" [{ required: true,
+        message: '请填写邮箱地址',trigger: ['blur'] }]">
+        <u-input placeholder="用于接收电子发票(必填)" v-model="formData.email"></u-input>
       </u-form-item>
       <u-form-item labelWidth="120" label="注册地址" prop="registerAddress">
-        <u-input placeholder="(选填) v-model=" formData.registerAddress"></u-input>
+        <u-input placeholder="(选填)" v-model=" formData.registerAddress"></u-input>
       </u-form-item>
       <u-form-item labelWidth="120" label="注册电话" prop="registerPhone">
-        <u-input v-model="formData.registerPhone" placeholder="(选填)"></u-input>
+        <u-input placeholder="(选填)" v-model="formData.registerPhone"></u-input>
       </u-form-item>
-      <u-form-item labelWidth="120" label="开户银行" prop="bank" placeholder="(选填)">
-        <u-input v-model="formData.bank"></u-input>
+      <u-form-item labelWidth="120" label="开户银行" prop="bank">
+        <u-input placeholder="(选填)" v-model="formData.bank"></u-input>
       </u-form-item>
-      <u-form-item labelWidth="120" label="银行账户" prop="bankAccount" placeholder="(选填)">
-        <u-input v-model="formData.bankAccount"></u-input>
+      <u-form-item labelWidth="120" label="银行账户" prop="bankAccount">
+        <u-input placeholder="(选填)" v-model="formData.bankAccount"></u-input>
       </u-form-item>
       <u-form-item labelWidth="120" label="默认发票抬头" prop="isDefault">
-        <u-switch v-model="formData.isDefault"></u-switch>
+        <u-switch class="flex u-flex-end" v-model="formData.isDefault"></u-switch>
       </u-form-item>
     </u-form>
   </view>
   <view class="save-button-wrap">
-    <up-button @click="goInvoiceAddOrEdit(false)" style="color: #F7F7F7" class="save-button" color="#2583B7"
-      shape="circle" text="添加发票抬头" />
+    <up-button @click="submitForm" style="color: #F7F7F7" class="save-button" color="#2583B7" shape="circle"
+      text="保存" />
   </view>
 </template>
 
@@ -48,15 +50,26 @@
   import {
     onLoad
   } from '@dcloudio/uni-app';
-
+  let showTaxPayer = ref(true);
   let isEdit = ref('');
   onLoad((options) => {
     console.log(options.isEdit, "options");
     isEdit.value = options.isEdit;
+
+
   })
+
+  // 基本案列数据
+  const radiolist = reactive([{
+      name: '个人/非企业单位',
+    },
+    {
+      name: '企业',
+    }
+  ]);
   const form = ref(null)
   const formData = ref({
-    headerType: '企业',
+    headerType: null,
     invoiceHeader: '',
     taxpayerId: '',
     email: '',
@@ -65,8 +78,10 @@
     bank: '',
     bankAccount: '',
     isDefault: false
-  })
+  });
 
+  formData.value.headerType = "企业";
+  showTaxPayer.value = true;
   const submitForm = () => {
     form.value.validate((valid) => {
       if (valid) {
@@ -76,7 +91,22 @@
         console.log('表单验证失败')
       }
     })
-  }
+  };
+
+
+  const groupChange = (n) => {
+    console.log('groupChange', n);
+    console.log(formData.headerType, "formData.headerType");
+  };
+
+  const radioChange = (n) => {
+    formData.headerType = n;
+    if (n === '企业') {
+      showTaxPayer.value = true;
+    } else {
+      showTaxPayer.value = false;
+    }
+  };
 </script>
 
 <style scoped lang="scss">
@@ -120,6 +150,11 @@
     color: #666666;
     line-height: 33rpx;
     text-align: left;
+  }
+
+  ::v-deep .u-form-item__body__right {
+    flex: 1;
+    padding-right: 20rpx;
   }
 </style>
 
